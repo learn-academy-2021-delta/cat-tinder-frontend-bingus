6000c47b3c8f8cfc6656020ef13729ceb511a7f2
alappie <ashley@specialtyproduce.com>
2021-10-27 13:45:23 -0700

    gimmy sum cat photos

---
+++ src/pages/NotFound.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+// import notCat from '../assets/not-cat.jpeg'
+
+class NotFound extends Component {
+  render() {
+    return (
+      <div className="page-body">
+        <h3>Ooops, no cats here!</h3>
+   
+      </div>
+    )
+  }
+}
+export default NotFound
