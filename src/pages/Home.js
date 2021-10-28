f59e2c51cab6dbcfeb5a966616dea3b5ec64cb63
Chawwh <chachablueftw@gmail.com>
2021-10-28 13:03:30 -0600

    Added some styling much wow very yes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 
+
 class Home extends Component {
   render() {
     return (
-      <>
-        <h3>I am the Home</h3>
-      </>
+      <div id="home-container">
+        <h3 className="home">I am the Home</h3>
+        <button onClick={this.props.alert}>Sign up Today!</button>
+        <button onClick={this.props.alert}>Log in</button>
+      </div>
     )
   }
 }
